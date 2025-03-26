a59e03e12c3ad880739de9f9bd5e77414c7fd4e0
hantong6 <hantong6@163.com>
2025-03-26 23:36:12 +0800

    follow

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use solana_sdk::transaction::Transaction;
 
 #[derive(Debug, BorshDeserialize, BorshSerialize)]
 pub enum SocialInstruction {
-    Init {seed_type: String},
+    Init(String),
     Follow(Pubkey),
     Unfollow(Pubkey),
     QueryFollows,
@@ -69,7 +69,7 @@ impl SocialClient {
 
     pub fn init_user(&self, user: &Keypair, seed_type: &str) -> Result<(), Box<dyn Error>> {
         let social_pda = get_social_pda(&self.program_id, &[user.pubkey().as_ref(), seed_type.as_bytes()]);
-        let init_user_data = SocialInstruction::Init{seed_type: seed_type.to_string()};
+        let init_user_data = SocialInstruction::Init(seed_type.to_string());
         let init_user_acc = vec![
             AccountMeta::new(user.pubkey(), true),
             AccountMeta::new(social_pda, false),
@@ -82,6 +82,38 @@ impl SocialClient {
         );
         let sign = self.send_instruction(user, vec![init_user_ins])?;
         println!("init user success, sign: {:?}", sign);
+        Ok(())
+    }
+
+    pub fn follow_user(&self, user: &Keypair, follow_user: Pubkey) -> Result<(), Box<dyn Error>> {
+        let social_pda = get_social_pda(&self.program_id, &[user.pubkey().as_ref(), USER_PROFILE_SEED.as_bytes()]);
+        let follow_user_data = SocialInstruction::Follow(follow_user);
+        let follow_user_acc = vec![
+            AccountMeta::new(social_pda, false),
+        ];
+        let follow_user_ins = Instruction::new_with_borsh(
+            self.program_id,
+            &follow_user_data,
+            follow_user_acc
+        );
+        let sign = self.send_instruction(user, vec![follow_user_ins])?;
+        println!("follow user success, sign: {:?}", sign);
+        Ok(())
+    }
+
+    pub fn query_follow(&self, user: &Keypair) -> Result<(), Box<dyn Error>> {
+        let social_pda = get_social_pda(&self.program_id, &[user.pubkey().as_ref(), USER_PROFILE_SEED.as_bytes()]);
+        let query_follow_data = SocialInstruction::QueryFollows;
+        let query_follow_acc = vec![
+            AccountMeta::new(social_pda, false),
+        ];
+        let query_follow_ins = Instruction::new_with_borsh(
+            self.program_id,
+            &query_follow_data,
+            query_follow_acc
+        );
+        let sign = self.send_instruction(user, vec![query_follow_ins])?;
+        println!("query follow success, sign: {:?}", sign);
         Ok(())
     }
 
@@ -108,9 +140,12 @@ fn get_social_pda(program_id: &Pubkey, seed: &[&[u8]]) -> Pubkey {
 fn main() -> Result<(), Box<dyn Error>> {
     // let user_profile = UserProfile::new();
     // print!("user profile len is {:?}", borsh::to_vec(&user_profile).unwrap().len());
-    let program_id = Pubkey::from_str("7DGy2um3GUoptaPYbKfAknhvsjYt97noMKEcHZw7Eqgf")?;
+    let program_id = Pubkey::from_str("AbiXdK7xj8T3HsgUPKxhYhNQJ8DsujgxeBz9Q8bcfPxu")?;
     let user = read_keypair_file("/home/hantong/.config/solana/id-local.json")?;
     let social_client = SocialClient::new("http://127.0.0.1:8899", program_id);
     social_client.init_user(&user, USER_PROFILE_SEED)?;
+    let follower_user = Pubkey::from_str("CAz782xYgu4q8zcg5VDafaRLCLDq6FiYPXFSqX5xQtWJ")?;
+    social_client.follow_user(&user, follower_user)?;
+    social_client.query_follow(&user)?;
     Ok(())
 }
